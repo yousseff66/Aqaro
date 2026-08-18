5f1ace309c1327b98253a489b2d70d6c1e23c7c0
yousseff66 <yousefalsafhy@gmail.com>
2026-08-18 17:07:12 +0300

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import FirebaseCore // 1. أضفنا ده
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    FirebaseApp.configure() // 2. أضفنا السطر ده عشان Firebase يشتغل نيتف
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
