92f0528607f3fa630701083b393e7b34b9e8b148
yousseff66 <yousefalsafhy@gmail.com>
2026-08-19 02:11:53 +0300

    Fix: Standardize iOS AppDelegate for auto-swizzling and bump version to 15

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,11 +1,10 @@
-import Flutter
 import UIKit
+import Flutter
 import FirebaseCore
-import FirebaseMessaging
 import GoogleMaps
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -13,46 +12,10 @@ import GoogleMaps
     // تهيئة خرائط جوجل
     GMSServices.provideAPIKey("AIzaSyAL-3NyNo1yug-JncZQwLFuqPhOZYjK1-w")
 
-    // تشغيل فايربيز
-    if FirebaseApp.app() == nil {
-        FirebaseApp.configure()
-    }
+    // تشغيل فايربيز (نترك الباقي للمكتبة تلقائياً)
+    FirebaseApp.configure()
 
-    // تعيين مفوض المراسلة
-    Messaging.messaging().delegate = self
-
-    // تسجيل الإشعارات
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self
-      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-      UNUserNotificationCenter.current().requestAuthorization(
-        options: authOptions,
-        completionHandler: { _, _ in }
-      )
-    }
-
-    application.registerForRemoteNotifications()
-
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // ربط توكن آبل بفايربيز
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-    Messaging.messaging().apnsToken = deviceToken
-    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-  }
-
-  override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    print("Failed to register for remote notifications: \(error.localizedDescription)")
-    super.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
-  }
-
-  // استقبال توكن فايربيز (للتأكيد)
-  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-    print("Firebase registration token: \(String(describing: fcmToken))")
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
