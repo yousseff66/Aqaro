12f49c48448e64f8ce72098ad23f5b2ae3ad60ba
yousseff66 <yousefalsafhy@gmail.com>
2026-08-19 01:49:51 +0300

    Fix: Finalize iOS push notification setup in AppDelegate and bump version to 13

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -23,7 +23,12 @@ import GoogleMaps
 
     // تسجيل الإشعارات
     if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      UNUserNotificationCenter.current().delegate = self
+      let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+      UNUserNotificationCenter.current().requestAuthorization(
+        options: authOptions,
+        completionHandler: { _, _ in }
+      )
     }
 
     application.registerForRemoteNotifications()
@@ -31,7 +36,7 @@ import GoogleMaps
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  // ربط توكن آبل بفايربيز (مهم جداً للايفون الحقيقي)
+  // ربط توكن آبل بفايربيز
   override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
     Messaging.messaging().apnsToken = deviceToken
     super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
